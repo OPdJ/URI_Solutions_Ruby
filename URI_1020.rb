181132c3cda01604971ac28098165db7d7eb9942
Otavio Pereira <otajesus@bol.com.br>
2017-08-24 11:06:20 -0300

    Accepted

---
+++ URI_1020.rb
@@ -0,0 +1,14 @@
+=begin
+    Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias
+
+    Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias. Nos 
+    casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 360, 363 ou 364. 
+    Este é apenas um exercício com objetivo de testar raciocínio matemático simples.
+=end
+
+valueA = gets.to_i
+
+puts "#{(valueA/365)} ano(s)"
+valueA = valueA%365
+puts "#{(valueA/30)} mes(es)"
+puts "#{(valueA%30)} dia(s)"
